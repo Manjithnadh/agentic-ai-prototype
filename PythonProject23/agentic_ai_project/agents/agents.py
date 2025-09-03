c55cce3cbbb036af485018005517c64c8d25abee
Manjithnadhgit config --global user.email manjithmullapudi@GMAIL.comgit config --global user.name Manjithnadh <manjithmullapudi@gmail.com>
2025-09-03 14:30:52 +0530

    commit

--- PythonProject23/agentic_ai_project/agents/agents.py
+++ PythonProject23/agentic_ai_project/agents/agents.py
@@ -1,12 +1,12 @@
-from langchain_community.tools import DuckDuckGoSearchRun
+
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.agents import initialize_agent, AgentType
+from langchain.agents import  AgentType
 from PythonProject23.agentic_ai_project.tools.db_tool import query_sqlite_db
-from langchain_community.utilities import SearxSearchWrapper
+
 from langchain_community.agent_toolkits import create_sql_agent
-from langchain.tools import Tool
+
 from langchain_community.utilities import SQLDatabase
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
+
 from PythonProject23.agentic_ai_project.tools import db_tool
 from dotenv import load_dotenv
 import os
@@ -31,7 +31,4 @@ agent_executor=create_sql_agent(
 
 db_tool.agent_executor=agent_executor
 
-query="what is best rating drugs"
 
-a=agent_executor.run(query)
-print(a)
